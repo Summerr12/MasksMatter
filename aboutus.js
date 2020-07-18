591e074647518aa89d0007f50bf955feedaf757d
Marvin Lee <43216761+malee31@users.noreply.github.com>
2020-07-17 22:23:33 -0700

    No longer mug shots

--- aboutus.js
+++ aboutus.js
@@ -2,6 +2,7 @@ const display = $(".description");
 const img = $(".example");
 $(".circle img").hover(function() {
 	let current = $(this).clone();
+	current.css("position", "relative");
 	display.html(`My name is ${current.attr("data-name")} and I am the ${current.attr("data-role")} of Mask Matters.`);
 	let clone = current.clone();
 	$(".center").remove();
