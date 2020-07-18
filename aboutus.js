9f3061c85f405f76450ad7b7a7bd8a9ac08f07fd
aychou <32346173+aychou@users.noreply.github.com>
2020-07-17 22:13:07 -0700

    yeyeyeye

--- aboutus.js
+++ aboutus.js
@@ -2,7 +2,8 @@ const display = $(".description");
 const img = $(".example");
 $(".circle img").hover(function() {
 	let current = $(this).clone();
-	display.html(`My name is ${current.attr("data-name")} and I am the ${current.attr("data-role")} of Mask Matters.`);
+	if (!current.attr("data-name")) return;
+	display.html(` My name is ${current.attr("data-name")}. I am the ${current.attr("data-role")}`);
 	let clone = current.clone();
 	$(".center").remove();
 	current.addClass("center");
