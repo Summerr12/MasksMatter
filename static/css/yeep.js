5a66d74fe9dc5bf6329197875429df6fd20d6186
aychou <32346173+aychou@users.noreply.github.com>
2021-09-03 00:34:42 -0700

    back image

--- static/css/yeep.js
+++
@@ -1,9 +0,0 @@
-const express=require("express")
-const app=express()
-const path=require("path")
-
-app.get("/", (req, res)=>{
-	res.sendFile(path.resolve(__dirname, "index.html"))
-})
-
-app.listen(process.env.PORT ||8000);
